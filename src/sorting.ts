41000de575867032e379bf2ab022e3995ee59ea0
Mathieu Magalhaes <mathieu.magalhaes@gmail.com>
2025-03-24 03:03:32 +0100

    feat: narrow down vue scope packages

--- src/sorting.ts
+++ src/sorting.ts
@@ -40,10 +40,10 @@ const parseSourceFolder = (srcFolder: string) => {
     }, [])
 
   const CUSTOM_GROUPS: Record<string, string[]> = {
-    '@/react': ['^react$', '^react-.*', '^@react/.*', '.*-react-.*'],
-    '@/vue': ['^vue$', '^vue-.*', '^@vue/.*', '.*-vue-.*'],
-    '@/vueuse': ['^vueuse$', '^vueuse-.*', '^@vueuse/.*', '.*-vueuse-.*'],
-    '@/nuxt': ['^nuxt$', '^nuxt-.*', '^@nuxt/.*', '.*-nuxt-.*'],
+    '@/react': ['^react$', '^react-.*', '^@react/.*'],
+    '@/vue': ['^vue$', '^vue-.*', '^@vue/.*'],
+    '@/vueuse': ['^vueuse$', '^vueuse-.*', '^@vueuse/.*'],
+    '@/nuxt': ['^nuxt$', '^nuxt-.*', '^@nuxt/.*'],
   }
 
   folders.forEach(({ folderName }) => {
